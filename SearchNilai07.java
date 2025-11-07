2b5f545da756ad02fca49b690d773dd31bf41841
FataRach <emillulfr@gmail.com>
2025-11-07 15:25:17 +0700

    Percobaan 4

--- SearchNilai07.java
+++ SearchNilai07.java
@@ -4,36 +4,19 @@ public class SearchNilai07 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
-    int [] arrNilai = {};
-    int key;
+    int [] arrNilai = {80, 85, 78, 96, 90, 82, 86};
+    int key = 90;
     int hasil = 0;
-    boolean ditemukan = false;
-
-    System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
-    int jumlah = input.nextInt();
-    arrNilai = new int[jumlah];
-
-    for (int i = 0; i < arrNilai.length; i++) {
-      System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
-      arrNilai[i] = input.nextInt();
-    }
-
-    System.out.print("Masukkan nilai yang ingin dicari: ");
-    key = input.nextInt();
 
     for (int i = 0; i < arrNilai.length; i++) {
       if (key == arrNilai[i]) {
         hasil = i;
-        ditemukan = true;
         break;
       }
     }
     System.out.println();
-
-    if (ditemukan) {
-    System.out.println("Nilai "+ key +" Ketemu!, Merupakan Nilai Mahasiswa Ke-" + (hasil + 1));
-    } else {
-    System.out.println("Nilai Tidak Ditemukan");
+    System.out.println("Nilai "+ key +" Ketemu Di Indeks Ke-" + hasil);
+    System.out.println();
     }
   }
-}
+
---
+++ SearchNilai07.md
@@ -0,0 +1,8 @@
+1. Jelaskan maksud dari statement break; pada baris ke-10 kode program percobaan 4 di atas. 
+2. Modifikasi kode program pada percobaan 4 di atas sehingga program dapat menerima input berupa banyaknya elemen array nilai, isi array, dan sebuah nilai (key) yang ingin dicari. Lalu cetak ke layar indeks posisi elemen dari nilai (key) yang dicari
+3. Modifikasi program pada percobaan 4 di atas, sehingga program akan memberikan pesan "Nilai yang dicari tidak ditemukan" jika nilai yang dicari (key) tidak ada di dalam array.
+
+Jawaban :   
+1. break; digunakan untuk menghentikan perulangan (loop), meskipun kondisi perulangan masih memungkinkan untuk berjalan.
+2. -
+3. -
